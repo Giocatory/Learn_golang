173179a7884c156a48b36bd2ebc54c7d76c4169a
Giocatory <nivolyas@gmail.com>
2025-06-02 14:53:59 +0300

    Generate random number

--- my_modules/get_random_number.go
+++ my_modules/get_random_number.go
@@ -1,13 +1,17 @@
 package my_modules
 
 import (
+	"errors"
 	"math/rand"
 	"time"
 )
 
-func GetRandomNumber(number int) int {
+func GetRandomNumber(number int) (int, error) {
+	if number < 0 {
+		return 0, errors.New("number is negative")
+	}
 	seconds := time.Now().Unix() // Получаем текущую дату и время в целом числе
 	rand.Seed(seconds)           // Функция генератор случайного числа
 	target := rand.Intn(number) + 1
-	return target
+	return target, nil
 }
