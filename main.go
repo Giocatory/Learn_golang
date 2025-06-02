7f6cc96630943f779cf69e90b6b68e9e94c6afd8
Giocatory <nivolyas@gmail.com>
2025-06-03 00:30:23 +0300

    проверка типа через рефлект

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"Learn_golang/my_modules"
 	"fmt"
+	"reflect"
 )
 
 func main() {
@@ -10,7 +11,7 @@ func main() {
 	intResult, _ := my_modules.GetMax(1, 2, 3, 4, 5)
 	floatResult, _ := my_modules.GetMax(1.1, 2.2, 3.3, 4.4, 5.5)
 
-	fmt.Println(stringResult)
-	fmt.Println(intResult)
-	fmt.Println(floatResult)
+	fmt.Println(stringResult, reflect.TypeOf(stringResult)) // e string
+	fmt.Println(intResult, reflect.TypeOf(intResult))       // 5 int
+	fmt.Println(floatResult, reflect.TypeOf(floatResult))   // 5.5 float64
 }
