e226d7380d00c1acc9dfa76eafe1d02a5cfae67d
Giocatory <nivolyas@gmail.com>
2025-06-02 22:32:58 +0300

    безопасный перебор массивов

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"Learn_golang/my_modules"
 	"fmt"
 )
 
 func main() {
-	resultInt := my_modules.GetAverageIntNumbers(1, 2, 3, 4, 5)
-	resultFloat := my_modules.GetAverageFloatNumbers(1.0, 2.0, 3.0, 4.0, 5.0)
+	myArray := [...]int{1, 2, 3, 4, 5}
 
-	fmt.Println(resultInt, resultFloat)
+	for index, value := range myArray {
+		fmt.Println(index, value)
+	}
 }
