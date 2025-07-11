36acfa2d41bf70aeb4eed9f5b2de8e29bddaa4d6
Giocatory <nivolyas@gmail.com>
2025-07-12 00:21:46 +0300

    первая русская буква

--- main.go
+++ main.go
@@ -3,11 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	x := 10
-	if x > 5 {
-		fmt.Println(x)
-		x := 5
-		fmt.Println(x)
-	}
-	fmt.Println(x)
+	name := "Михаил"
+	var firstLatter rune = []rune(name)[0]
+	fmt.Println(string(firstLatter))
 }
